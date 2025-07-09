69e1b23311dc8febd9bb661e711a7cdee6630457
trs1141 <trs1141@yahoo.co.jp>
2025-07-09 16:55:41 +0900

    ハンバーガーメニューをきれいにした

---
+++ src/app/components/ArticleListTest.tsx
@@ -0,0 +1,16 @@
+// import ArticleList from './ArticleList';
+
+type PostMeta = {
+  title: string;
+  date: string;
+  category: string;
+  slug: string;
+  thumbnail?: string;
+  excerpt: string;
+};
+
+// ダミーデータ完全削除済み
+
+export default function ArticleListTest() {
+  return null;
+}
