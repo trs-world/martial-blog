dcfe37b719dc62171601271ea057e2dbc8fc08f3
trs1141 <trs1141@yahoo.co.jp>
2025-07-11 18:41:05 +0900

    Google Adsence審査用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4014848226068727"
+          crossOrigin="anonymous"></script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <div className="topbar" style={{
           width: '100vw',
